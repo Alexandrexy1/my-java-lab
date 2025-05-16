252ba22f4e2733b8555071303bc5fe250dad24d4
Alexandre <alexandrenas2003@gmail.com>
2025-05-16 19:07:54 -0300

    day01 while loop

--- day01/src/Main.java
+++ day01/src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         String name = "Alexandre";
+        int number = 0;
 //        Iterando através do laço for
 //        for(int i = 0; i < name.length(); i++) {
 //            System.out.println(name.charAt(i));
 //        }
+        while (number < name.length()) {
+            System.out.println(name.charAt(number));
+            number++;
+        }
+
     }
 }
