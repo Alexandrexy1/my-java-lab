3c5514d748401aab8d7d7641af01be5c8ff72944
Alexandre <alexandrenas2003@gmail.com>
2025-05-16 19:01:52 -0300

    day01 for loop

---
+++ day01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alexandre";
+//        Iterando através do laço for
+//        for(int i = 0; i < name.length(); i++) {
+//            System.out.println(name.charAt(i));
+//        }
+    }
+}
