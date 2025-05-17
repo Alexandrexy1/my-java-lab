e7143a5054416aa696cf3413106ef38acaa45cec
Alexandre <alexandrenas2003@gmail.com>
2025-05-17 15:22:09 -0300

    day02 outputtools print and println

---
+++ day02/outputtools/OutputTools.java
@@ -0,0 +1,10 @@
+package day02.outputtools;
+
+public class OutputTools {
+    public static void main(String[] args) {
+
+        System.out.print("Hello");
+        System.out.println(", guys!");
+        //output: Hello, guys!
+    }
+}
