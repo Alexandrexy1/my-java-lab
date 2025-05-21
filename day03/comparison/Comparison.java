1193857f8f1852ce87bf41cd6b53fe70bb66b911
Alexandre <alexandrenas2003@gmail.com>
2025-05-21 16:51:37 -0300

    comparison

--- day03/comparison/Comparison.java
+++ day03/comparison/Comparison.java
@@ -1,4 +1,26 @@
-package day03.comparison;
+import java.util.Scanner;
 
 public class Comparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1, number2;
+
+        System.out.print("Enter first integer: "); // 3
+        number1 = sc.nextInt();
+
+        System.out.print("Enter second integer: "); // 5
+        number2 = sc.nextInt();
+
+        if (number1 == number2) System.out.printf("%d == %d%n", number1, number2);
+
+        if (number1 != number2) System.out.printf("%d != %d%n", number1, number2); // 3 != 5
+
+        if (number1 < number2) System.out.printf("%d < %d%n", number1, number2); // 3 < 5
+
+        if (number1 > number2) System.out.printf("%d > %d%n", number1, number2);
+
+        if (number1 <= number2) System.out.printf("%d <= %d%n", number1, number2); // 3 <= 5
+
+        if (number1 >= number2) System.out.printf("%d >= %d%n", number1, number2);
+    }
 }
