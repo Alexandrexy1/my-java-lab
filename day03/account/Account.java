d8f574db93864177ccc4cd6ed9a5bca1c2d0d5ee
Alexandre <alexandrenas2003@gmail.com>
2025-06-05 16:25:27 -0300

    day04

--- day03/account/Account.java
+++ day03/account/Account.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Account {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
